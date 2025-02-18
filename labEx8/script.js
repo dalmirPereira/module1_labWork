d6f7281cc87b33d731cb62d031338fa018c77a1b
Dalmir Pereira <dalmir_pereira@outlook.com>
2025-02-18 16:37:56 +1100

    Modularization of  labEx8/script.js (rollDice())

--- labEx8/script.js
+++ labEx8/script.js
@@ -11,14 +11,13 @@ function getDice() {
     // Inspect the value and type of the variable that will be used in random number generation.
     console.log(`this is the value grabbed from <select>: ${numFaces}, and type: ${typeof numFaces}`);
 
-    return rollDice(numFaces);
+    // Display the result of the dice roll in the "diceResult" span
+    const result = rollDice(numFaces)
+    document.getElementById('diceResult').innerText = result;
 }
 
 function rollDice(numFaces){
     // Generate a random number between 1 and the number of faces on the selected dice
     // Math.random() generates a number between 0 and 1, and Math.floor() rounds it down
-    const result = Math.floor(Math.random() * numFaces) + 1;  
-
-    // Display the result of the dice roll in the "diceResult" span
-    document.getElementById('diceResult').innerText = result;
+    return Math.floor(Math.random() * numFaces) + 1;    
 }
